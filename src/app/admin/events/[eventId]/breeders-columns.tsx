e13cebc734ef3b99c226defb1708a1e52dd8aab7
Dragoon4002 <sounak.dey2468@gmail.com>
2026-02-17 23:40:15 +0530

    Added status to bird model, name changes from entry fee to perch fee

--- src/app/admin/events/[eventId]/breeders-columns.tsx
+++ src/app/admin/events/[eventId]/breeders-columns.tsx
@@ -63,14 +63,14 @@ export const createBreedersColumns = (
     }
   },
   {
-    header: "Perch Fees Value",
+    header: "Purge Fees Value",
     cell: ({row})=>{
         const perchFeesValue = row.original.payments.filter(p => p.paymentType === 'PERCH_FEE').reduce((sum, p) => sum + p.amountToPay, 0);
         return <span>${perchFeesValue.toFixed(2)}</span>;
     }
   },
   {
-    header: "Perch Fees Paid",
+    header: "Purge Fees Paid",
     cell: ({row})=>{
         const perchFeesPaid = row.original.payments.filter(p => p.paymentType === 'PERCH_FEE').reduce((sum, p) => sum + p.amountPaid, 0);
         return <span>${perchFeesPaid.toFixed(2)}</span>;
