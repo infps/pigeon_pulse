2f378dea2ddb646de0d0966de69e9b8ddaf66a4b
Dragoon4002 <sounak.dey2468@gmail.com>
2026-02-18 09:07:15 +0530

    Prisma + Next.js Turbopack compatibility issue resolved for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@prisma/client", "prisma"],
   images:{
     remotePatterns:[
       new URL('https://pub-a39a3b5950f34f50bb2158b6cf756558.r2.dev/**')
